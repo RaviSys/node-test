bb8012349585cb37c6b4788a77e07f76ac264ad7
Ravi Kumar Singh <rksingh@isystango.com>
2019-03-05 16:54:51 +0530

    add body-parser

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var bodyparser = require('body-parser')
 var mw = require('./middlewares/configurable.js')
 app.set('view engine', 'pug');
 app.set('views','./views');
@@ -24,10 +25,23 @@ app.use(requestTime)
 
 app.use(mw({option1: 1, option2: 2, option3: 3}))
 
+app.use( bodyparser.json() )
+app.use(bodyparser.urlencoded({
+  extended: true
+})); 
+
+app.use(express.json())
+app.use(express.urlencoded())
+
+
 app.get('/', function(req, res) {
   res.render('index', {title: "Express Practice", message: "Hello Express"});
 });
 
+app.get('/register', (req, res) => {
+  console.log(req.body.name)
+  res.render('auth/register', { title: 'Register Account | Express Practice', message: 'Register Here' })
+});
 
 
 app.listen(8080);
